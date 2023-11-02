a2419f4cb1fe73abd05a6723b750bf58d1147a3d
dao0203 <youzaizuoteng872@gmail.com>
2023-11-02 21:29:47 +0900

    add expect declaration for num and set ios and android

--- shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
@@ -1,9 +1,13 @@
 package com.example.kmpapp
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+//        return "Hello, ${platform.name}!"
+        return "$firstWord $num, ${platform.name}!"
     }
 }
