e2fba34bd2a8c66edf7d6dc04fabaa49b25ba8d1
dao0203 <youzaizuoteng872@gmail.com>
2023-11-04 17:44:08 +0900

    add dayUntilNewYear function and set to greet list

--- shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
@@ -5,8 +5,11 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
+    private val daysPhrase = "These are only ${daysUntilNewYear()} days until New Year! 🎉"
+
     fun greet(): List<String> = buildList {
         add("Hello, ${platform.name}!")
         add("Random number: ${platform.name}")
+        add(daysPhrase)
     }
 }
