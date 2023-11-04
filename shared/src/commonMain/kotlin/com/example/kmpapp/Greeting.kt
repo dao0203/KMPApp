28edc6c5972e702593bd883ca5b6edebb51f0f56
dao0203 <youzaizuoteng872@gmail.com>
2023-11-04 18:48:08 +0900

    change return type of the greet function to flow

--- shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
@@ -1,15 +1,32 @@
 package com.example.kmpapp
 
+import com.example.kmpapp.data.RocketComponent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 class Greeting {
     private val platform: Platform = getPlatform()
 
     private val daysPhrase = "These are only ${daysUntilNewYear()} days until New Year! 🎉"
 
-    fun greet(): List<String> = buildList {
-        add("Hello, ${platform.name}!")
-        add("Random number: ${platform.name}")
-        add(daysPhrase)
+    private val rocketComponent = RocketComponent()
+
+//    fun greet(): List<String> = buildList {
+//        add("Hello, ${platform.name}!")
+//        add("Random number: ${platform.name}")
+//        add(daysPhrase)
+//    }
+
+    fun greet(): Flow<String> = flow {
+        emit (if (Random.nextBoolean()) "Hi!" else "Hello!")
+        delay(1.seconds)
+        emit("Guess what it is! > ${platform.name.reversed()}")
+        delay(1.seconds)
+        emit(daysPhrase)
+        delay(1.seconds)
+        emit(rocketComponent.launchPhrase())
     }
 }
