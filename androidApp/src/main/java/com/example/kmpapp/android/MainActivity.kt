20735e135332a44f4ef248b858ec295924d688e6
dao0203 <youzaizuoteng872@gmail.com>
2023-11-04 17:30:00 +0900

    change screen because of changing return types of greet function

--- androidApp/src/main/java/com/example/kmpapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmpapp/android/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.kmpapp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kmpapp.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -27,14 +34,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView(phrases: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(phrases.size) { index ->
+            Text(phrases[index])
+            Divider()
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView(listOf("Hello, Android!"))
     }
 }
--- shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmpapp/Greeting.kt
@@ -5,9 +5,8 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
-//        return "Hello, ${platform.name}!"
-        return "$firstWord $num, ${platform.name}!"
+    fun greet(): List<String> = buildList {
+        add("Hello, ${platform.name}!")
+        add("Random number: ${platform.name}")
     }
 }
